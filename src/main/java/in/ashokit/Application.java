0bd86cb08d86fad5325ddb8786a3153ac33a0df9
AshadKhan-786 <69005333+AshadKhan-786@users.noreply.github.com>
2024-04-30 15:51:35 +0530

    Update Application.java

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -6,12 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	// AAA-101
-	public void m1() {
-		int x = 10;
-		// logic in-progress
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
